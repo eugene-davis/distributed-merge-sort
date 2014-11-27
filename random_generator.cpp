bd5237555f2a638fc7f99487c25af02b706cf329
Eugene Davis <eugene_davis@industrialdragonfly.com>
2014-11-27 15:30:45 -0600

    Updated random number generator to take command line arguments and write to a file.

--- random_generator.cpp
+++ random_generator.cpp
@@ -11,23 +11,38 @@
 
 using namespace std;
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	int dataSize = 0;
-	while (dataSize < 50000000 || dataSize > 140000000)
+
+	// Handle command line arguments
+	if (argc != 3)
 	{
-		cout << "Enter a data size between 50 000 000 and 140 000 000." << endl;
+		cout << "Incorrect usage. Correct invocation is: " << endl;
+		cout << "random_generator num_data_points output_file_name" << endl;
+		cout << "The number of data points must be between 50 000 000 and 140 000 000." << endl;
+		return 0;
+	}
 	
-		cin >> dataSize;
-		if (dataSize < 50000000 && dataSize > 140000000)
-		{
-			cout << "Number not in range, try again." << endl;
-		}
+	// Get argument for data size
+	dataSize = atoi(argv[1]);
+	// Check that it was a valid data size
+	if (dataSize < 50000000 || dataSize > 140000000)
+	{
+		cout << "Requested number of data points not within range." << endl;
+		return 0;
 	}
 
 	// Setup file operations
 	ofstream dataFile;
-	dataFile.open("rand_nums.dat");
+	// Open file for writing, and truncate it so any previous content is removed
+	dataFile.open(argv[2], ofstream::out | ofstream::trunc);
+
+	if (!dataFile.is_open())
+	{
+		cout << "Failed to open data file for writing." << endl;
+		return 0;
+	}
 
 	// Seed random number generator
 	srand(time(NULL));
@@ -38,4 +53,6 @@ int main(void)
 		// Write out the number, each number is on a new line
 		dataFile << rand() << endl;
 	}	
+
+	dataFile.close();
 }
