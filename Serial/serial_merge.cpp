602d8f59236dbba48d35232ad883a903cdcf8641
Eugene Davis <eugene_davis@industrialdragonfly.com>
2014-11-27 15:58:09 -0600

    Wrote first part of serial merge, opening the file and reading in the size of the data

---
+++ Serial/serial_merge.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char *argv[])
+{
+	int dataSize = 0;
+
+	// Parse commandline argument
+	if (argc != 2)
+	{
+		cout << "Incorrect usauge. Correct invocation is: " << endl;
+		cout << "serial_merge input_file_name" << endl;
+		return 0;
+	}
+
+	// Open the file
+	ifstream dataFile;
+	dataFile.open(argv[1]);
+	
+	// Check if file opened correctly
+	if (!dataFile.is_open())
+	{
+		cout << "Failed to open data file for reading." << endl;
+		return 0;
+	}
+
+	// Get size of data (from first line of file)
+	dataFile >> dataSize;
+
+	cout << dataSize << endl;
+}
