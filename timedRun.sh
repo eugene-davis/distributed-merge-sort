dd78a313e4828515649d17f91da99da122d7863a
Eugene Davis <eugene_davis@industrialdragonfly.com>
2014-11-30 01:32:04 -0600

    Made improvements to timedRun

--- timedRun.sh
+++ timedRun.sh
@@ -7,21 +7,26 @@ dataSetName="data.dat"
 projectDir="/home/student/emd0003/Live_Project"
 
 #Deploy the clients first thing so they have time to launch
+echo "Deploying clients for distributed run"
 cd $projectDir/Distributed/
 bash deploy.sh
 
 # Generated new data set
+echo "Generating random data set of size $dataSetSize"
 cd $projectDir/gen_data
 ./random_generator $dataSetSize $dataSetName
 
-# Time run for serial version
+# Timed run for serial version
+echo "Running serial version"
 cd $projectDir/Serial/
 time ./serial_merge $dataSetName
 
-# Run distributed version
+# Time run for distributed version
+echo "Running distributed server"
 cd $projectDir/Distributed/
 time ./server_emd0003 $dataSetName
 
 # Cleanup
+echo "Cleaning up data set files"
 cd $projectDir/gen_data
 rm *dat
