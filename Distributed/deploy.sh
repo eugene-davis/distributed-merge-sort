57e1327cf1f80dda3502ef2b49056852344066d1
Eugene Davis <eugene_davis@industrialdragonfly.com>
2014-11-30 01:48:30 -0600

    Added accidently removed backgrounding &

--- Distributed/deploy.sh
+++ Distributed/deploy.sh
@@ -6,5 +6,5 @@ clientList="ClientList.conf"
 while read host 
 do
 	echo "Deploying client to $host"
-	ssh emd0003@$host ~/Live_Project/Distributed/client_emd0003
+	ssh emd0003@$host ~/Live_Project/Distributed/client_emd0003 &
 done < $clientList
